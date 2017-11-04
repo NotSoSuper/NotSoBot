4b4b1114ad0dae23c3c79740fb02b9ebb74e2ca9
matmen <gcmatmen@gmail.com>
2017-11-04 17:02:51 +0100

    delete this

--- steam/__init__.py
+++
@@ -1,5 +0,0 @@
-from .steamid              import SteamId
-from .steamprofile         import SteamProfile
-
-from .steamaccountuniverse import SteamAccountUniverse
-from .steamaccounttype     import SteamAccountType
--- steam/steamaccountuniverse.py
+++
@@ -1,6 +0,0 @@
-class SteamAccountUniverse(object):
-	Invalid  = 0
-	Public   = 1
-	Beta     = 2
-	Internal = 3
-	Dev      = 4
